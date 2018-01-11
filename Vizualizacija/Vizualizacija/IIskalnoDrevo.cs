2d0f3b2e3b17b89951ddffb27ee3e20900ab40ba
martincesnovar <martin.cesnovar@gmail.com>
2018-01-12 00:42:57 +0100

    vizualizacija

---
+++ Vizualizacija/Vizualizacija/IIskalnoDrevo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vizualizacija
+{
+    interface IIskalnoDrevo<T>
+    {
+        void Vstavi(T el);
+        void Izbrisi(T el);
+        void Isci(T el);
+    }
+}
